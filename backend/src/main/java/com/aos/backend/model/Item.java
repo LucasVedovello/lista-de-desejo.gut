7cf38c633cf23098cb421fbf726c4b22794b3981
lucasvedovello <lucasscaranovedovello@gmail.com>
2026-09-22 08:52:29 -0300

    Model
    Model criado.

--- backend/src/main/java/com/aos/backend/model/Item.java
+++ backend/src/main/java/com/aos/backend/model/Item.java
@@ -1,3 +1,4 @@
+
 package com.aos.backend.model;
 
 import jakarta.persistence.Column;
@@ -5,15 +6,59 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
+@Table (name = "wish")
 public class Item {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false, length = 100)
+    private String preco;
+
     @Column(nullable = false, length = 500)
-    private String title;
+    private String prioridade;
+
+ @Column(nullable = false)
+    private boolean comprado;
+
+    public Item() {
+        this.preco = "";
+        this.comprado = false; 
+    }
+      public Item(String preco, String prioridade){
+        this.preco = preco;
+        this.prioridade = prioridade;
+        this.comprado = false;
+    }
+    public Long getId(){
+        return id;
+    }
+    public String getPreco(){
+        return preco;
+    }
+    public String getPrioridade(){
+        return prioridade;
+    }
+    public boolean getComprado(){
+        return comprado;
+    }
+    public void setId(Long id){
+        this.id = id;
+    }
+    public void setPreço(String preco){
+        this.preco = preco;
+    }
+    public void setPrioridade(String prioridade){
+        this.prioridade = prioridade;
+    }
+    public void setComprado(boolean comprado){
+        this.comprado = comprado;
+    }
+
+
 
 }
