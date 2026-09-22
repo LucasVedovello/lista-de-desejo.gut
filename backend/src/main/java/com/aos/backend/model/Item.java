5a2b9092c69ccae1cefc3e18c9c9e6056200dc38
lucasvedovello <lucasscaranovedovello@gmail.com>
2026-09-22 08:38:12 -0300

    Update Item.java
    Model criado

--- backend/src/main/java/com/aos/backend/model/Item.java
+++ backend/src/main/java/com/aos/backend/model/Item.java
@@ -1,8 +1,19 @@
 package com.aos.backend.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
-@Entity 
+@Entity
 public class Item {
-    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 500)
+    private String title;
+
 }
